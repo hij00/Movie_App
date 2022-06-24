e9be844abfb82f6759fbe724df17778a04d97146
hijeong_ <gmlwjdchl82@naver.com>
2022-06-24 11:36:14 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,54 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { mainStyle } from "../styles/globalStyle";
+
+const SHeader = styled.div`
+  max-width: 100%;
+  width: 100%;
+  padding: 0 80px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Logo = styled.h3`
+  font-size: 28px;
+  font-weight: 800;
+  a {
+    color: ${mainStyle.mainColor};
+  }
+`;
+
+const MenuWrap = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Menu = styled.li`
+  margin-left: 100px;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
 export const Header = () => {
-  return <>Header</>;
+  return (
+    <SHeader>
+      <Logo>
+        <Link to={"/"}>Movie</Link>
+      </Logo>
+      <MenuWrap>
+        <Menu>
+          <Link to={"/"}>홈</Link>
+        </Menu>
+        <Menu>
+          <Link to={"/search"}>영화검색</Link>
+        </Menu>
+      </MenuWrap>
+    </SHeader>
+  );
 };
